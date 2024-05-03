ec109909c0a0265eecac8a909258a105ce1e4c7b
Abderrahmane Yahiaoui <76871067+Abdo30004@users.noreply.github.com>
2024-05-03 23:53:38 +0100

    Init

---
+++ src/test/java/org/gp/rem/RemApplicationTests.java
@@ -0,0 +1,13 @@
+package org.gp.rem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
