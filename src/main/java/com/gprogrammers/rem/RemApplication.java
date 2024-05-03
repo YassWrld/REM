70d49ee036926d2b7a86110223e6eec280e4bb49
Abderrahmane Yahiaoui <76871067+Abdo30004@users.noreply.github.com>
2024-05-03 23:57:07 +0100

    Init

--- src/main/java/com/gprogrammers/rem/RemApplication.java
+++ src/main/java/com/gprogrammers/rem/RemApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class RemApplication {
 
+
     public static void main(String[] args) {
         SpringApplication.run(RemApplication.class, args);
     }
