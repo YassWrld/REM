4757dad580e400818ae166c6db598304ca048ee0
Yass <yacineoubyi@gmail.com>
2024-05-04 10:19:51 +0100

    Init

---
+++ src/main/java/com/gprogrammers/rem/RemApplication.java
@@ -0,0 +1,13 @@
+package com.gprogrammers.rem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gprogrammers/rem/RemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gprogrammers.rem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
