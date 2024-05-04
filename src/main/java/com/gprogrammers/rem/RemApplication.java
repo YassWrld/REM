3e46c4b705672663b444b50ded18f2e6a5bf40ae
Abderrahmane Yahiaoui <76871067+Abdo30004@users.noreply.github.com>
2024-05-04 11:54:30 +0100

    Removing / route

--- src/main/java/com/gprogrammers/rem/RemApplication.java
+++ src/main/java/com/gprogrammers/rem/RemApplication.java
@@ -2,21 +2,13 @@ package com.gprogrammers.rem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
 public class RemApplication {
 
 
     public static void main(String[] args) {
         SpringApplication.run(RemApplication.class, args);
-    }
-
-    @GetMapping("/")
-    public String hello() {
-        return "Hello World!";
     }
 
 }
